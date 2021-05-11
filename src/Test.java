11c8bcb430d26f1dca115343ae6f85da3cce409b
MGN1106 <2278493494@qq.com>
2021-05-11 21:58:55 +0800

    1

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class Test2{
-}
-class Test {
-    public static void main(String[] args) {
-        new Test2();//error 如下图
-    }
-}
---
+++ src/practice/Shape.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.util.concurrent.Callable;
+
+public abstract class Shape {
+    public abstract double getArea();
+}
+class Circle extends Shape {
+    public double getArea() {
+        return (10);
+    }
+    static class Rectangle extends Shape{
+        public double getArea(){
+            return(10);
+        }
+        static class Square extends Rectangle{
+            public double getArea(){
+                return(10);
+            }
+        }
+        public static void main(String args[]){
+            Circle circle;
+            circle=new Circle();
+            System.out.println("圆的面积"+circle.getArea());
+            Rectangle rectangle;
+            rectangle=new Rectangle();
+            System.out.println("长方形的面积"+rectangle.getArea());
+            Square square;
+            square=new Square();
+            System.out.println("正方形的体积"+square.getArea());
+            System.out.println("正方形是特殊的长方形");
+
+
+                }
+            }
+        }
