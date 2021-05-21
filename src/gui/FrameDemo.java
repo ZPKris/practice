30796f92b1aad611ebf5c76fe3256e3285de8383
MGN1106 <2278493494@qq.com>
2021-05-21 20:47:49 +0800

    1

---
+++ src/gui/FrameDemo.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+public class FrameDemo {
+    public static void main(String[] args) {
+        JFrame f2=new JFrame("hello");
+        MyFrame frame1=new MyFrame("第一个窗口");
+        frame1.setLocation(400,400);
+        MyFrameDate frame2=new MyFrameDate("第二个窗口");
+        frame2.showTime();
+
+
+    }
+}
